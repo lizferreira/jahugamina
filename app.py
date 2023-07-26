84101a687278cf1fb99064bd8e189fce31a07e9d
Liz <96144994+lizferreira@users.noreply.github.com>
2023-07-26 14:28:39 -0400

    actualizadoo

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
+#importar flask y render template
 from flask import Flask, render_template
 
+#crear una variable con la clase flask
 app=Flask(__name__)
 
+#creacion de rutas
+
+#base de la header y el fouter
 @app.route("/")
 def ruta_principal():
     return render_template("base.html")
 
+#ruta de la home page
+@app.route("/homepage")
+def homepage():
+    return render_template("homepage.html")
+
+#ruta de la landing page
 @app.route('/landingpage')
 def landing():
     return render_template("landingpage.html")
+
+
+
+
 
 
  
